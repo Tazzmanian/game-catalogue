d8e28d3a5754a4829b876ef60a58e17f696b6230
Tazzmanian <banskaliqtabg@gmail.com>
2022-12-21 11:23:50 +0200

    Init project

---
+++ src/main/java/com/catalogue/gamecatalogue/GameCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.catalogue.gamecatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalogue/gamecatalogue/GameCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogue.gamecatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
